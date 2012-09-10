6941d544138899eb985d898d3a1aefa8b4edf5c8
amaxilat <amaxilat@cti.gr>
2012-09-10 15:45:51 +0300

    add observe capabilities

--- src/main/java/eu/uberdust/coap/udp/CoapUdpRequestHandler.java
+++ src/main/java/eu/uberdust/coap/udp/CoapUdpRequestHandler.java
@@ -1,24 +1,32 @@
 package eu.uberdust.coap.udp;
 
 import ch.ethz.inf.vs.californium.coap.MediaTypeRegistry;
+import ch.ethz.inf.vs.californium.coap.Message;
 import ch.ethz.inf.vs.californium.coap.Option;
+import ch.ethz.inf.vs.californium.coap.OptionNumberRegistry;
 import ch.ethz.inf.vs.californium.coap.Request;
 import ch.ethz.inf.vs.californium.coap.Response;
 import eu.uberdust.coap.CoapServer;
 import eu.uberdust.coap.NodeHandler;
 import eu.uberdust.coap.PropertyReader;
+import eu.uberdust.coap.UberdustNodeHandler;
+import eu.uberdust.communication.websocket.readings.WSReadingsClient;
 import org.apache.log4j.Logger;
 
 import java.io.InputStream;
 import java.net.DatagramPacket;
+import java.net.SocketAddress;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Random;
 
 /**
  * Handler Thread fro the COAP upd packets of the server.
  */
-public class CoapUdpRequestHandler implements Runnable {//NOPMD
+public class CoapUdpRequestHandler implements Runnable, Observer {//NOPMD
 
     /**
      * LOGGER.
@@ -26,6 +34,9 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
     private static final Logger LOGGER = Logger.getLogger(CoapUdpRequestHandler.class);
 
     private transient final DatagramPacket packet;
+    private String node;
+    private String capability;
+    private Request udpRequest;
 
     /**
      * Constructor.
@@ -39,14 +50,18 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
     @Override
     public void run() {
         final byte[] inData = cleanupData(packet.getData());
-        final Request udpRequest = (Request) Request.fromByteArray(inData);
+        udpRequest = (Request) Request.fromByteArray(inData);
 
         List<Option> udpOptions = udpRequest.getOptions();
         for (Option udpOption : udpOptions) {
             LOGGER.info("udp-option: " + udpOption.getName());
         }
 
+
         String requestURI = udpRequest.getUriPath();
+        if (udpRequest.getType() == Message.messageType.CON) {
+//            sendAck(udpRequest.getMID(), packet.getSocketAddress());
+        }
 
         LOGGER.info(requestURI);
         if (requestURI.equals("/.well-known/core")) {
@@ -78,6 +93,7 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
             replyPacket.setSocketAddress(packet.getSocketAddress());
             LOGGER.info(packet.getSocketAddress());
             CoapServer.getInstance().sendReply(replyPacket);
+            LOGGER.info("done");
 
         } else {
 
@@ -85,8 +101,8 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
             try {
                 String[] uriparts = requestURI.split("/");
                 if (uriparts.length == 3) {
-                    String node = uriparts[1];
-                    String capability = uriparts[2];
+                    node = uriparts[1];
+                    capability = uriparts[2];
                     URL uberdustReading = new URL(PropertyReader.getInstance().getProperties().get("uberdustURL") + "/node/" + node + "/capability/" + capability + "/latestreading");
                     uberdustReading.openConnection();
                     final InputStream input = uberdustReading.openStream();
@@ -105,6 +121,15 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
                     response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
                     response.setPayload(parts[1]);
 
+                    List<Option> options = udpRequest.getOptions();
+
+
+
+                    if (udpRequest.hasOption(OptionNumberRegistry.OBSERVE)) {
+                        UberdustNodeHandler.getInstance().observe(node, capability, this);
+                        response.setToken(udpRequest.getToken());
+                    }
+
 
                     byte[] buf = new byte[1024];
                     final DatagramPacket replyPacket = new DatagramPacket(buf, 1024);
@@ -119,8 +144,25 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
         }
     }
 
+    private void sendAck(int mid, SocketAddress socketAddress) {
+        final Message ack = new Message(Message.messageType.ACK, 0);
+        ack.setMID(mid);
+
+
+        byte[] buf = new byte[1024];
+        final DatagramPacket replyPacket = new DatagramPacket(buf, 1024);
+        replyPacket.setData(ack.toByteArray());
+        replyPacket.setSocketAddress(socketAddress);
+        LOGGER.info(packet.getSocketAddress());
+
+        LOGGER.info("EmptyACK=" + ack.isEmptyACK());
+        CoapServer.getInstance().sendReply(replyPacket);
+
+    }
+
     private Response createResponse(Request udpRequest) {
         Response response = new Response();
+
 
         response.setURI(udpRequest.getUriPath());
         response.setMID(udpRequest.getMID());
@@ -153,5 +195,50 @@ public class CoapUdpRequestHandler implements Runnable {//NOPMD
         System.arraycopy(data, 0, adata, 0, myLen);
         LOGGER.info(adata.length);
         return adata;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        if ((node == null) || (capability == null)) {
+            return;
+        }
+        if (!(o instanceof WSReadingsClient)) {
+            return;
+        }
+        if (!(arg instanceof eu.uberdust.communication.protobuf.Message.NodeReadings)) {
+            return;
+        }
+        eu.uberdust.communication.protobuf.Message.NodeReadings readings = (eu.uberdust.communication.protobuf.Message.NodeReadings) arg;
+        for (eu.uberdust.communication.protobuf.Message.NodeReadings.Reading reading : readings.getReadingList()) {
+            if (reading.hasDoubleReading()) {
+                notifyFor(String.valueOf(reading.getDoubleReading()));
+            } else if (reading.hasStringReading()) {
+                notifyFor(reading.getStringReading());
+            }
+        }
+    }
+
+    private void notifyFor(String stringReading) {
+
+        Response response = createResponse(udpRequest);
+        response.setContentType(MediaTypeRegistry.TEXT_PLAIN);
+        response.setPayload(stringReading);
+
+        if (udpRequest.hasOption(OptionNumberRegistry.OBSERVE)) {
+            UberdustNodeHandler.getInstance().observe(node, capability, this);
+            response.setToken(udpRequest.getToken());
+        }
+        Random rand = new Random();
+
+        response.setMID(rand.nextInt());
+
+        byte[] buf = new byte[1024];
+        final DatagramPacket replyPacket = new DatagramPacket(buf, 1024);
+        replyPacket.setData(response.toByteArray());
+        replyPacket.setSocketAddress(packet.getSocketAddress());
+        LOGGER.info(packet.getSocketAddress());
+        CoapServer.getInstance().sendReply(replyPacket);
+
+
     }
 }
